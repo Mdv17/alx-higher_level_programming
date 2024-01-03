86a99e910ad61ae1a6cc5dbd5777682882939a6e
Mdv17 <methembedvundla583@gmail.com>
2024-01-03 20:07:16 +0200

    rest of the task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+
+#include lists.h
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the head of the list
+ * Return: 0 if no cycle, 1 if cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *slow, *fast; /* pointers for traversal */
+
+    if (list == NULL) /* empty list has no cycle */
+        return (0);
+
+    slow = list; /* initialize slow pointer to head */
+    fast = list->next; /* initialize fast pointer to next node */
+
+    while (slow != NULL && fast != NULL && fast->next != NULL)
+    {
+        if (slow == fast) /* pointers meet at some node */
+            return (1); /* cycle found */
+        slow = slow->next; /* move slow pointer one node ahead */
+        fast = fast->next->next; /* move fast pointer two nodes ahead */
+    }
+
+    return (0); /* no cycle */
+}
+
---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,4 @@
+def magic_calculation(a, b):
+    result = 98 # LOAD_CONST 1 (98)
+    result += a ** b # BINARY_POWER, BINARY_ADD
+    return result # RETURN_VALUE
